3119a59cfaf0469128a69c87d44ac2e9a6e884db
Ronen Ofir <rofir2010@gmail.com>
2020-03-27 11:48:53 +0300

    validate uniq mail & phone

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,4 +7,13 @@ class Customer
   field :phone, type: String
   field :address, type: String
   field :age, type: Integer
+
+  # index({mail:1}, {unique:true, name:"mail_index"})
+  # index({phone:1}, {unique:true, name:"phone_index"})
+
+  validates_uniqueness_of :mail, :phone
+
+  validates_presence_of :first_name, :last_name, :mail, :phone
+
+
  end
